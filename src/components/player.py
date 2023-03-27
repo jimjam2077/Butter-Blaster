0264f38f5bbd43bf4279dd7f3005840ca25c6dc8
jimjam2077 <james.nicholas.thomas@gmail.com>
2023-03-27 23:52:26 +0100

    player image and movement

--- src/components/player.py
+++ src/components/player.py
@@ -1,21 +1,33 @@
-import pygame
+import pygame as pg
+from src.config import Config
 
 # passing Sprite makes Player Class a child of Sprite
 # can then use super().__init__() to call the Sprite's init() function
-class Player(pygame.sprite.Sprite):
-    
+
+
+class Player(pg.sprite.Sprite):
+
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load() #add image here
-        self.rect = self.image.get_rect() #defines the borders according to image size
-        self.rect.center = () #defines a starting position for rect
-        
-    
+        self.image = pg.image.load("assets/playership.png")  # add image here
+        self.rect = self.image.get_rect()  # defines the borders according to image size
+        self.rect.center = ()  # defines a starting position for rect
+
     def update(self):
-        
+        pressed_keys = pg.key.get_pressed()
+        if pressed_keys[pg.K_UP]:
+            self.rect.move_ip(0, -5)  # y grows down, x grows right
+        if pressed_keys[pg.K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if (self.rect.left > 0): # dont move off screen
+            if pressed_keys[pg.K_LEFT]:
+                self.rect.move_ip(-5, 0)
+        if(self.rec.right < Config.WIDTH):
+            if pressed_keys[pg.K_RIGHT]:
+                self.rect.move_ip(5, 0)
+
     def draw(self, screen):
-        #can add other things to draw here
+        # can add other things to draw here
         screen.blit(self.image, self.rect)
-        
+
     def reset(self):
-        
--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+# could make this a more secure format
+# but it will do for now!
 class Config:
     FPS = 60;
     
@@ -8,3 +10,10 @@ class Config:
     #movement
     ACC = 1.2
     FRIC = -0.1
+    
+    # Predefined some colors
+    BLUE  = (0, 0, 255)
+    RED   = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
