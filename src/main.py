19f4ad337881102dfa458722b17a9af80be40fb0
jimjam2077 <james.nicholas.thomas@gmail.com>
2023-04-18 15:45:42 +0100

    refactoring, changing game to a class

--- src/main.py
+++ src/main.py
@@ -1,22 +1,15 @@
 # import the pygame module, so you can use it
 import pygame as pg
 from config import Config
+from gameplay.game import Game
 from utils.state import State
-
+#from gameplay.game_screens import run_start_screen, run_char_screen, run_story_screen, run_game_screen,  run_game_over, run_game_won, get_game_state
+pg.init() # initialize the pygame module
 
 def main():
     clock = pg.time.Clock()
-    pg.init() # initialize the pygame module
-    
-    
-    # create a surface on screen that has the size of 240 x 180
-    #screen = pg.display.set_mode((1280, 720), pg.HWSURFACE | pg.DOUBLEBUF)  # w, h
     Config.setup_window()
-    from gameplay.game_screens import run_start_screen, run_char_screen, run_story_screen, run_game_screen,  run_game_over, run_game_won, get_game_state
-
-
-
-
+    game = Game()
     # main loop
     running = True
     clock.tick(Config.FPS)
@@ -25,13 +18,13 @@ def main():
         # calculate delta time in seconds
         current_tick = pg.time.get_ticks()
         dt = (current_tick - last_tick) / 1000.0
-        state = get_game_state()
+        state = game.get_game_state()
         if(state== State.START):
-            run_start_screen(dt)
+            game.run_start_screen(dt)
         elif(state == State.CHAR):
-            run_char_screen()
+            game.run_char_screen()
         elif (state == State.PLAYING):
-            run_game_screen(clock)
+            game.run_game_screen(clock)
         #update display
         pg.display.update()
         last_tick = current_tick
